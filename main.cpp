d7f1893cc4c7256f42fbf7b87549d39f69f8d647
yonghangtian <tianyh96@qq.com>
2023-11-22 19:17:26 +0800

    finished c++ primer's chapter 4, 22 Nov 2022

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 // #include "CppPrimerPlus/chapter17/chapter17.h"
 // #include "CppPrimerPlus/chapter18.h"
 // #include "CppPrimer5thEdtion/chapter2/chapter2.h"
-#include "CppPrimer5thEdtion/chapter3/chapter3.h"
+// #include "CppPrimer5thEdtion/chapter3/chapter3.h"
+#include "CppPrimer5thEdtion/chapter4/chapter4.h"
 
 int main(int, char**) 
 {
-    int a = exercise3_42();
+    int a = exercise4_37();
     
     return a;
 }
