3484c35cdba54843f032f381efd45be4378d810c
yonghangtian <tianyh96@qq.com>
2023-12-09 20:01:10 +0800

    Finished c++ primer's chapter 6 at Dec 9 2023.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 // #include "CppPrimerPlus/chapter17/chapter17.h"
 // #include "CppPrimerPlus/chapter18.h"
 // #include "CppPrimer5thEdtion/chapter2/chapter2.h"
-#include "CppPrimer5thEdtion/chapter5/chapter5.h"
+#include "CppPrimer5thEdtion/chapter6/chapter6.h"
 
-int main(int, char**) 
+int main(int argc, char **argv)
 {
-    int a = exercise5_25();
-    
+    int a = exercise6_55();
+    // int a = exercise6_26(argc, argv);
+
     return a;
 }
 
